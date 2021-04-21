cbc305142cfbb9850af21138bbc38533f092708a
justjoshriley <joshriley316@gmail.com>
2021-04-21 14:15:43 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+let myInfo = new TypeIt('#myStats', {
+    speed: 50
+})
+.type('Hey there!')
+// .pause(300)
+.break()
+.break()
+.type('My name is Josh. I\'m a <em> Computer Science </em>student at <em>Make School</em> in San Francisco.')
+.break()
+.break()
+.type('Welcome to my portfolio: my playground of interactions, features, and work that I have accomplished.')
+.break()
+.break()
+.type('Click around and be curious, I hope something here inspires you.')
+.break()
+.break()
+.type('- JWR')
+.go();
