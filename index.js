2cb74c03a75c6bfd94d84dbfd67cbecee11769d5
justjoshriley <joshriley316@gmail.com>
2021-04-28 15:25:10 -0700

    added themes

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ let myInfo = new TypeIt('#myStats', {
     speed: 50
 })
 .type('Hey there!')
-// .pause(300)
 .break()
 .break()
-.type('My name is Josh. I\'m a <em> Computer Science </em>student at <em>Make School</em> in San Francisco.')
+.type('My name is Joshua Riley. I\'m a <em> Computer Science </em>student at <em>Make School</em> in San Francisco.')
 .break()
 .break()
 .type('Welcome to my portfolio: my playground of interactions, features, and work that I have accomplished.')
@@ -16,3 +15,17 @@ let myInfo = new TypeIt('#myStats', {
 .break()
 .type('- J.W.R')
 .go();
+
+const themeSelector = document.querySelector("#themes");
+const themeLink = document.querySelector(".theme");
+
+function setTheme() {
+    let theme = themeSelector.value;
+    themeLink.setAttribute("href", "theme-" + theme + ".css");
+}
+
+themeSelector.addEventListener("change", () => {
+    setTheme();
+});
+
+setTheme();
