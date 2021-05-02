dbbad5f4a241f994ff2fb3baa18968838468f4ba
justjoshriley <joshriley316@gmail.com>
2021-05-01 17:12:35 -0700

    finished landing, hero, and step 1 UI

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ let myInfo = new TypeIt('#myStats', {
 const themeSelector = document.querySelector("#themes");
 const themeLink = document.querySelector(".theme");
 
+
 function setTheme() {
     let theme = themeSelector.value;
     themeLink.setAttribute("href", "theme-" + theme + ".css");
